0bea59eee62ac41f4c3b3b6e12b2cd3b7fb144e1
Tzuzul <mail@tzuzulcode.com>
2022-08-17 22:02:25 -0500

    SSR, CSR y SSG

--- ecommerce/pages/contact.jsx
+++ ecommerce/pages/contact.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function contact() {
   return (
-    <div>contact</div>
+    <div>
+      
+    </div>
   )
 }
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [click,setClick] = useState(1)
   return (
     <>
       <h1>Hola mundo, next.js</h1>
+      <button onClick={()=>{setClick(click+1)}}>{click}</button>
     </>
   )
 }

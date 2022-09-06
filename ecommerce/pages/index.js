b3cfff3a1f7452177af7eeac233aa748884dbabc
Alejandro <alejandrom.am33@gmail.com>
2022-09-06 13:17:17 -0500

    add: context for auth

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,31 +1,38 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import Link from "next/link"
 import Navbar from '../components/Navbar'
 import { signOut } from 'firebase/auth'
 import { auth } from '../libs/firebase'
+import { authContext } from '../context/AuthContext'
 
 // Estática
 export default function Home() {
-  const [click,setClick] = useState(1)
+  const {user, setUser} = useContext(authContext)
+
+  console.log(user); 
 
   const logout = ()=>{
     signOut(auth)
+    setUser({
+      logout: true
+    })
   }
   return (
     <>
       <Navbar/>
-      <h1>Hola mundo, next.js</h1>
-      <button onClick={()=>{setClick(click+1)}}>{click}</button>
-      <button onClick={logout}>Log out</button>
 
-      <Link href="/login">Login</Link>
-      <Link href="/signup">SignUp</Link>
-      <Link href="/pokemon">Pokemon</Link>
-      <Link href="/pokemon_ssr">Pokemon SSR</Link>
-      <Link href="/products">Products</Link>
-      <Link href="/admin/products/create">Create Product</Link>
+      <div className='flex gap-3'>
+        {!user.logout && <button onClick={logout}>Log out</button>}
+        {user.logout && <Link href="/login">Login</Link>}
+        {user.logout && <Link href="/signup">SignUp</Link>}
+        <Link href="/pokemon">Pokemon</Link>
+        <Link href="/pokemon_ssr">Pokemon SSR</Link>
+        <Link href="/products">Products</Link>
+        <Link href="/admin/products/create">Create Product</Link>
+        {!user.logout && <h1>Welcome {user.name}!</h1>}
+      </div>
     </>
   )
 }
